f5cb81c6c877f03e0e6cca1b32ac2b5e22657a35
Bagas Yanuar Sudrajad <37441075+bagasys@users.noreply.github.com>
2020-02-22 22:58:11 +0700

    Navbar

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import './App.css';
+import NavBar from './components/navbar/NavBar'
+import Search from './components/search/Search'
+
+function App() {
+  return (
+    <MuiThemeProvider>
+      <div>
+      <NavBar/>
+      <Search/>
+      </div>
+      
+    </MuiThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import AppBar from 'material-ui/AppBar'
+const NavBar = () => {
+    return (
+        <AppBar title='Image Finder'/>
+    )
+}
+
+export default NavBar;
